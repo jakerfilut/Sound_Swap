47f027f5b56137bcea99325136aedf064573af9c
Francesca Hamm <francesca.c.hamm@gmail.com>
2022-01-27 14:03:32 -0700

    added hms converter to songs on playlist duration

--- Sound_Swap/client/src/pages/Playlist.js
+++ Sound_Swap/client/src/pages/Playlist.js
@@ -4,6 +4,25 @@ function Playlist() {
   const [songs, setSongs] = useState([]);
   const [updateSong, setUpdateSong] = useState(false);
 
+  const convertHMS = (value) => {
+    const sec = parseInt(value, 10); // convert value to number if it's string
+    let hours = Math.floor(sec / 3600); // get hours
+    let minutes = Math.floor((sec - hours * 3600) / 60); // get minutes
+    let seconds = sec - hours * 3600 - minutes * 60; //  get seconds
+    // add 0 if value < 10; Example: 2 => 02
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    // console.log(hours + ":" + minutes + ":" + seconds);
+    return minutes + ":" + seconds; // Return is HH : MM : SS
+  };
+
   useEffect(() => {
     fetch("/songs")
       .then((res) => res.json())
@@ -28,7 +47,7 @@ function Playlist() {
         <h1 className="song_title">{song.title}</h1>
         <img src={song.image} />
         <div>{song.artist}</div>
-        <div>{song.duration}</div>
+        <div>{convertHMS(song.duration)}</div>
         <button onClick={() => handleDelete(song)}>Remove From Playlist</button>
       </div>
     );
